2f6f9d93bfed70f74b25c31b7c128f370a12afba
JUSELEROstudent <juansebastialeon5@gmail.com>
2022-09-10 13:11:37 -0500

    se crea el nuevo controlador que funciona para el chat
    sigue implementar elsignal r

---
+++ apitest/Controllers/ChatUsers.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using apitest.Services;
+using Dapper;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+
+namespace apitest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ChatUsers : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetFriends()
+        {            
+            var stream = HttpContext.Request.Headers.Authorization.ToString();
+            var handler = new JwtSecurityTokenHandler();
+            var stream2 = stream.Remove(0, 7);
+            var jsonToken = handler.ReadToken(stream2);
+            var tokenS = jsonToken as JwtSecurityToken;
+            var GetToUser = tokenS.Payload["unique_name"];
+            var conectionable = new ConnectionSql();
+            using (var queryable = conectionable.CreateConnection())
+            {
+                queryable.Open();
+                string userInsertuery = "SELECT userId, name  FROM usertesting";
+                var rowsAffected = queryable.Query<responsepic>(userInsertuery).ToList();
+                return Ok(rowsAffected);
+            }
+        }
+    }
+
+    public class responsepic
+    {
+        public string name { get; set; }
+        
+        public int userId { get; set; }
+
+        public string pic { get; set; }
+    
+    }
+}
